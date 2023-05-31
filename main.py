17203da9e504d1ad56243710e1e6523be052e7c3
Sheep26 <crabeykiller@gmail.com>
2023-05-31 12:12:27 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-Speed = 75
+def on_received_value(name, value):
+    if name == "move":
+        if value == 1:
+            servos.P0.run(speed)
+        if value == 2:
+            servos.P1.run(speed)
+    if name == "stopall":
+        servos.P0.run(0)
+        servos.P1.run(0)
+    if name == "stop":
+        if value == 1:
+            servos.P0.run(0)
+        if value == 2:
+            servos.P1.run(0)
+radio.on_received_value(on_received_value)
+
+speed = 0
+speed = 75
+radio.set_group(2)
 
 def on_forever():
     if input.button_is_pressed(Button.A):
-        servos.P0.run(Speed)
+        servos.P0.run(speed)
     if input.button_is_pressed(Button.B):
-        servos.P1.run(Speed)
+        servos.P1.run(speed)
     if not (input.button_is_pressed(Button.A)):
         servos.P0.run(0)
     if not (input.button_is_pressed(Button.B)):
