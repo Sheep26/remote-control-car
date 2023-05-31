c5b1768750f3b1c2af0e669026ffc4a7d27ed86f
Sheep26 <crabeykiller@gmail.com>
2023-05-31 12:07:47 +1200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,28 @@
-let speed = 75
+radio.onReceivedValue(function (name, value) {
+    if (name == "move") {
+        if (value == 1) {
+            servos.P0.run(speed)
+        }
+        if (value == 2) {
+            servos.P1.run(speed)
+        }
+    }
+    if (name == "stopall") {
+        servos.P0.run(0)
+        servos.P1.run(0)
+    }
+    if (name == "stop") {
+        if (value == 1) {
+            servos.P0.run(0)
+        }
+        if (value == 2) {
+            servos.P1.run(0)
+        }
+    }
+})
+let speed = 0
+speed = 75
+radio.setGroup(2)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         servos.P0.run(speed)
