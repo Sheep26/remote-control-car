99e925b903b45f07541e82f88785027c6ad20c86
Sheep26 <crabeykiller@gmail.com>
2023-10-22 20:13:03 +1300

    writing whole project to use bluetooth

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
-def on_received_value(name, value):
-    if name == "move":
-        if value == 1:
-            servos.P0.run(speed)
-        if value == 2:
-            servos.P1.run(-speed)
-        if value == 3:
-            servos.P0.run(speed)
-            servos.P1.run(-speed)
-    if name == "stopall":
-        servos.P0.stop()
-        servos.P1.stop()
-    if name == "stop":
-        if value == 1:
-            servos.P0.stop()
-        if value == 2:
-            servos.P1.stop()
-radio.on_received_value(on_received_value)
+def on_bluetooth_connected():
+    basic.show_leds("""
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        . . . . .
+        """)
+    basic.pause(2000)
+    basic.clear_screen()
+bluetooth.on_bluetooth_connected(on_bluetooth_connected)
 
-speed = 0
+def on_bluetooth_disconnected():
+    basic.show_leds("""
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        """)
+    basic.pause(2000)
+    basic.clear_screen()
+bluetooth.on_bluetooth_disconnected(on_bluetooth_disconnected)
+
+def on_uart_data_received():
+    global btdata
+    btdata = bluetooth.uart_read_until(serial.delimiters(Delimiters.NEW_LINE))
+    serial.write_line(btdata)
+bluetooth.on_uart_data_received(serial.delimiters(Delimiters.NEW_LINE),
+    on_uart_data_received)
+
+btdata = ""
 speed = 100
 
 def on_forever():
-    radio.set_group(2)
-    if False:
-        if input.button_is_pressed(Button.A):
-            servos.P0.run(speed)
-        if input.button_is_pressed(Button.B):
-            servos.P1.run(0 - speed)
-        if not (input.button_is_pressed(Button.A)):
-            servos.P0.stop()
-        if not (input.button_is_pressed(Button.B)):
-            servos.P1.stop()
+    if input.button_is_pressed(Button.A):
+        servos.P0.run(speed)
+    if input.button_is_pressed(Button.B):
+        servos.P1.run(0 - speed)
+    if not (input.button_is_pressed(Button.A)):
+        servos.P0.stop()
+    if not (input.button_is_pressed(Button.B)):
+        servos.P1.stop()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,45 +1,42 @@
-radio.onReceivedValue(function (name, value) {
-    if (name == "move") {
-        if (value == 1) {
-            servos.P0.run(speed)
-        }
-        if (value == 2) {
-            servos.P1.run(0 - speed)
-        }
-        if (value == 3) {
-            servos.P0.run(speed)
-            servos.P1.run(0 - speed)
-        }
-    }
-    if (name == "stopall") {
-        servos.P0.stop()
-        servos.P1.stop()
-    }
-    if (name == "stop") {
-        if (value == 1) {
-            servos.P0.stop()
-        }
-        if (value == 2) {
-            servos.P1.stop()
-        }
-    }
+bluetooth.onBluetoothConnected(function () {
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        . . . . .
+        `)
+    basic.pause(2000)
+    basic.clearScreen()
 })
-let speed = 0
-speed = 100
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    btdata = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    serial.writeLine(btdata)
+})
+let btdata = ""
+let speed = 100
 basic.forever(function () {
-    radio.setGroup(2)
-    if (false) {
-        if (input.buttonIsPressed(Button.A)) {
-            servos.P0.run(speed)
-        }
-        if (input.buttonIsPressed(Button.B)) {
-            servos.P1.run(0 - speed)
-        }
-        if (!(input.buttonIsPressed(Button.A))) {
-            servos.P0.stop()
-        }
-        if (!(input.buttonIsPressed(Button.B))) {
-            servos.P1.stop()
-        }
+    if (input.buttonIsPressed(Button.A)) {
+        servos.P0.run(speed)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        servos.P1.run(0 - speed)
+    }
+    if (!(input.buttonIsPressed(Button.A))) {
+        servos.P0.stop()
+    }
+    if (!(input.buttonIsPressed(Button.B))) {
+        servos.P1.stop()
     }
 })
